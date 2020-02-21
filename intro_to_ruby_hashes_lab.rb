11673992f1e016d2f17452ba33db4d6cd0f05811
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-21 14:25:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,8 @@ def monopoly_with_second_tier
   monopoly = {}
   monopoly[:railroads] = {}
   monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
   
 end
 
